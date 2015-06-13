3911cbb73c16cfa27cace5ab55c8f26f60a974ba
cmzmasek <cmzmasek@yahoo.com>
2015-06-12 18:25:30 -0700

    starting with documentation

--- cxio/src/org/cxio/AspectElement.java
+++ cxio/src/org/cxio/AspectElement.java
@@ -1,7 +1,19 @@
 package org.cxio;
 
+/**
+ * The interface for all AspectElements.
+ * 
+ * 
+ * @author cmzmasek
+ *
+ */
 public interface AspectElement {
 
+    /**
+     * This returns the name of the Aspect. 
+     * 
+     * @return the name of the Aspect
+     */
     public String getAspectName();
 
 }

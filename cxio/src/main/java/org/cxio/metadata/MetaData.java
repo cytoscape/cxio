1bb0210f3ec68215fd55443dd4a0bf3c68abd011
cmzmasek <cmzmasek@yahoo.com>
2015-09-16 15:20:33 -0700

    added java doc

--- cxio/src/main/java/org/cxio/metadata/MetaData.java
+++ cxio/src/main/java/org/cxio/metadata/MetaData.java
@@ -15,25 +15,43 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
- * This class is to hold various meta data and to serialize this
- * data to json, and the de-serialize from json.
+ * This class is to hold a list of MetaDataElements to serialize this
+ * data to json, and to de-serialize from json.
  *
  * @author cmzmasek
  *
  */
 public final class MetaData {
 
+    /**
+     * The name of the MetaDataElement list when serialized to json.
+     *
+     */
     public final static String                    NAME = "metaData";
     final private List<SortedMap<String, Object>> _data;
 
+    /**
+     * Constructor, to create an empty MetaData object.
+     *
+     */
     public MetaData() {
         _data = new ArrayList<SortedMap<String, Object>>();
     }
 
+    /**
+     * This method id to add a MetaDataElement
+     *
+     * @param e a MetaDataElement
+     */
     public final void addMetaDataElement(final MetaDataElement e) {
         _data.add(e.getData());
     }
 
+    /**
+     * This is to get the meta data as list of MetaDataElements
+     *
+     * @return a list of MetaDataElements
+     */
     public final List<MetaDataElement> asListOfMetaDataElements() {
         final ArrayList<MetaDataElement> l = new ArrayList<MetaDataElement>();
         for (int i = 0; i < size(); i++) {
@@ -42,18 +60,41 @@ public final class MetaData {
         return l;
     }
 
+    /**
+     * This is to get the meta data as list of sorted maps (String to Object).
+     *
+     * @return a list of sorted maps (String to Object)
+     */
     public final List<SortedMap<String, Object>> getMetaData() {
         return _data;
     }
 
+    /**
+     * This is to get a MetaDataElement.
+     *
+     *
+     * @param index the index of the MetaDataElement
+     * @return a MetaDataElement
+     */
     public final MetaDataElement getMetaDataElement(final int index) {
         return new MetaDataElement(_data.get(index));
     }
 
+    /**
+     * This returns the number of MetaDataElements.
+     *
+     * @return the number of MetaDataElements
+     */
     public final int size() {
         return _data.size();
     }
 
+    /**
+     * This is the serialize this MetaData object to json.
+     *
+     * @param w a JsonWriter
+     * @throws IOException
+     */
     public final void toJson(final JsonWriter w) throws IOException {
         w.writeObject(this);
     }
@@ -70,16 +111,37 @@ public final class MetaData {
         return sb.toString();
     }
 
+    /**
+     * This is to create a MetaData object from a json formatted InputStream.
+     *
+     * @param is a json formatted stream
+     * @return a MetaData object
+     * @throws IOException
+     */
     public final static MetaData createInstanceFromJson(final InputStream is) throws IOException {
         final ObjectMapper m = new ObjectMapper();
         return m.readValue(is, MetaData.class);
     }
 
+    /**
+     * This is to create a MetaData object from a JsonParser.
+     *
+     * @param is a JsonParser
+     * @return a MetaData object
+     * @throws IOException
+     */
     public static MetaData createInstanceFromJson(final JsonParser jp) throws JsonParseException, JsonMappingException, IOException {
         final ObjectMapper m = new ObjectMapper();
         return m.readValue(jp, MetaData.class);
     }
 
+    /**
+     * This is to create a MetaData object from a json formatted String.
+     *
+     * @param str a json formatted String
+     * @return a MetaData object
+     * @throws IOException
+     */
     public final static MetaData createInstanceFromJson(final String str) throws IOException {
         final ObjectMapper m = new ObjectMapper();
         return m.readValue(str, MetaData.class);
--- cxio/src/main/java/org/cxio/metadata/MetaDataElement.java
+++ cxio/src/main/java/org/cxio/metadata/MetaDataElement.java
@@ -7,9 +7,16 @@ import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
-import org.cxio.util.Util;
 import org.cxio.core.interfaces.AspectElement;
+import org.cxio.util.Util;
 
+/**
+ * This is to hold meta data.
+ * It is intended to be used together with class MetaData.
+ *
+ * @author cmzmasek
+ *
+ */
 public class MetaDataElement {
 
     public final static String              CONSISTENCY_GROUP = "consistencyGroup";
@@ -22,14 +29,31 @@ public class MetaDataElement {
 
     final private SortedMap<String, Object> _data;
 
+    /**
+     * Constructor to create an empty MetaDataElement.
+     *
+     */
     public MetaDataElement() {
         _data = new TreeMap<String, Object>();
     }
 
+    /**
+     * Constructor to create a MetaDataElement containing the
+     * data provided in a SortedMap (String to Object).
+     *
+     */
     public MetaDataElement(final SortedMap<String, Object> data) {
         _data = data;
     }
 
+    /**
+     * Convenience method to add a "property" (a map mapping String keys
+     * to String values).
+     *
+     * @return a "property" as map mapping String keys
+     * to String values
+     *
+     */
     public final void addProperty(final Map<String, String> property) {
         if (!_data.containsKey(PROPERTIES)) {
             _data.put(PROPERTIES, new ArrayList<Map<String, String>>());
@@ -37,75 +61,177 @@ public class MetaDataElement {
         getProperties().add(property);
     }
 
+    /**
+     * The get the Object value corresponding to a given key.
+     *
+     * @param key
+     * @return
+     */
     public final Object get(final String key) {
         return _data.get(key);
     }
 
-    public final void getConsistencyGroup() {
-        _data.get(CONSISTENCY_GROUP);
+    /**
+     * Convenience method to get the consistency group.
+     *
+     * @return the consistency group
+     */
+    public final Long getConsistencyGroup() {
+        return Long.valueOf((String) _data.get(CONSISTENCY_GROUP));
     }
 
+    /**
+     * This allows to get all he data in this object as a SortedMap (String to Object).
+     *
+     * @return a SortedMap (String to Object)
+     */
     public final SortedMap<String, Object> getData() {
         return _data;
     }
 
-    public final void getElementCount() {
-        _data.get(ELEMENT_COUNT);
+    /**
+     * Convenience method to get the element count.
+     *
+     * @return the element count
+     */
+    public final Long getElementCount() {
+        return Long.valueOf((String) _data.get(ELEMENT_COUNT));
     }
 
-    public final void getIdCounter() {
-        _data.get(ID_COUNTER);
+    /**
+     * Convenience method to get the id counter.
+     *
+     * @return the id counter
+     */
+    public final Long getIdCounter() {
+        return Long.valueOf((String) _data.get(ID_COUNTER));
     }
 
-    public final void getLastUpdate() {
-        _data.get(LAST_UPDATE);
+    /**
+     * Convenience method to get the last update value.
+     *
+     * @return the last update value
+     */
+    public final String getLastUpdate() {
+        return (String) _data.get(LAST_UPDATE);
     }
 
-    public final void getName() {
-        _data.get(NAME);
+    /**
+     * Convenience method to get the name (of the corresponding aspect).
+     *
+     * @return the name (of the corresponding aspect)
+     */
+    public final String getName() {
+        return (String) _data.get(NAME);
     }
+
+    /**
+     * Convenience method to add a "property" (a map mapping String keys
+     * to String values).
+     *
+     * @return a "property" as map mapping String keys
+     * to String values
+     *
+     */
 
     @SuppressWarnings("unchecked")
+    /**
+     * Convenience method to get all "properties" (maps mapping String keys
+     * to String values).
+     *
+     * @return  all "properties"
+     */
     public final ArrayList<Map<String, String>> getProperties() {
         return ((ArrayList<Map<String, String>>) _data.get(PROPERTIES));
     }
 
-    public final void getVersion() {
-        _data.get(VERSION);
+    /**
+     * Convenience method to get the (corresponding aspect) version.
+     * 
+     * @return the (corresponding aspect) version
+     */
+    public final String getVersion() {
+        return (String) _data.get(VERSION);
     }
 
+    /**
+     * This returns the keys for all the data fields.
+     *
+     * @return the keys for all the data fields
+     */
     public final Set<String> keySet() {
         return _data.keySet();
     }
 
+    /**
+     * This is to put an arbitrary Object as value (data field), mapped to a key.
+     *
+     * @param key the key
+     * @param value an Object
+     */
     public final void put(final String key, final Object value) {
         _data.put(key, value);
     }
 
+    /**
+     * Convenience method to set the consistency group.
+     *
+     * @param c
+     */
     public final void setConsistencyGroup(final Long c) {
-        _data.put(CONSISTENCY_GROUP, c);
+        _data.put(CONSISTENCY_GROUP, String.valueOf(c));
     }
 
+    /**
+     * Convenience method to set the element count.
+     *
+     * @param c
+     */
     public final void setElementCount(final Long c) {
-        _data.put(ELEMENT_COUNT, c);
+        _data.put(ELEMENT_COUNT, String.valueOf(c));
     }
 
+    /**
+     * Convenience method to set the id counter.
+     *
+     * @param c
+     */
     public final void setIdCounter(final Long c) {
-        _data.put(ID_COUNTER, c);
+        _data.put(ID_COUNTER, String.valueOf(c));
     }
 
+    /**
+     * Convenience method to set the last update value.
+     *
+     * @param last_update
+     */
     public final void setLastUpdate(final String last_update) {
         _data.put(LAST_UPDATE, last_update);
     }
 
+    /**
+     * Convenience method to set the name (of the corresponding aspect).
+     *
+     * @param the name (of the corresponding aspect)
+     */
     public final void setName(final String name) {
         _data.put(NAME, name);
     }
-    
+
+    /**
+     * Convenience method to set the name from an AspectElement.
+     *
+     * @param e an AspectElement (to get the name from)
+     */
     public final void setName(final AspectElement e) {
         _data.put(NAME, e.getAspectName());
     }
 
+    /**
+     * Convenience method to set the (corresponding aspect) version.
+     *
+     * @param the (corresponding aspect) version
+     */
     public final void setVersion(final String version) {
         _data.put(VERSION, version);
     }

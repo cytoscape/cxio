b36662ef3cbd9b3269ca0e0c9fdebe7fce2e299e
cmzmasek <cmzmasek@yahoo.com>
2015-09-29 13:29:27 -0700

    UTF8 test

---
+++ cxio/src/main/java/org/cxio/examples/ExamplesUTF8.java
@@ -0,0 +1,141 @@
+package org.cxio.examples;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.cxio.aspects.datamodels.AbstractAttributesAspectElement.ATTRIBUTE_TYPE;
+import org.cxio.aspects.datamodels.CartesianLayoutElement;
+import org.cxio.aspects.datamodels.EdgeAttributesElement;
+import org.cxio.aspects.datamodels.EdgesElement;
+import org.cxio.aspects.datamodels.NodeAttributesElement;
+import org.cxio.aspects.datamodels.NodesElement;
+import org.cxio.aspects.readers.CartesianLayoutFragmentReader;
+import org.cxio.aspects.readers.EdgeAttributesFragmentReader;
+import org.cxio.aspects.readers.EdgesFragmentReader;
+import org.cxio.aspects.readers.NodeAttributesFragmentReader;
+import org.cxio.aspects.readers.NodesFragmentReader;
+import org.cxio.core.AspectElementCounts;
+import org.cxio.core.CxReader;
+import org.cxio.core.CxWriter;
+import org.cxio.core.interfaces.AspectElement;
+import org.cxio.core.interfaces.AspectFragmentReader;
+import org.cxio.util.Util;
+
+public class ExamplesUTF8 {
+
+    public static void main(final String[] args) throws IOException {
+
+        // Creating same AspectElements and adding them to Lists (representing
+        // AspectFragments)
+        // --------------------------------------------------------------------
+        final List<AspectElement> edges_elements = new ArrayList<AspectElement>();
+        edges_elements.add(new EdgesElement("ひらがな", "한글", "chữ Quốc ngữ"));
+        edges_elements.add(new EdgesElement("อักษรไทย", "漢字", "カタカナ"));
+
+        final List<AspectElement> nodes_elements = new ArrayList<AspectElement>();
+        nodes_elements.add(new NodesElement("漢字"));
+        nodes_elements.add(new NodesElement("chữ Quốc ngữ"));
+        nodes_elements.add(new NodesElement("カタカナ"));
+
+        final List<AspectElement> cartesian_elements = new ArrayList<AspectElement>();
+        cartesian_elements.add(new CartesianLayoutElement("漢字", 12, 21, 1));
+        cartesian_elements.add(new CartesianLayoutElement("chữ Quốc ngữ", 42, 23, 2));
+        cartesian_elements.add(new CartesianLayoutElement("カタカナ", 34, 23, 3));
+
+        final EdgeAttributesElement ea0 = new EdgeAttributesElement("ひらがな", "한글", "☃", ATTRIBUTE_TYPE.STRING);
+        final EdgeAttributesElement ea1 = new EdgeAttributesElement("ひらがな", "weight", "☎", ATTRIBUTE_TYPE.INTEGER);
+        final EdgeAttributesElement ea2 = new EdgeAttributesElement("อักษรไทย", "한글", "☔", ATTRIBUTE_TYPE.STRING);
+        final EdgeAttributesElement ea3 = new EdgeAttributesElement("อักษรไทย", "weight", "♨", ATTRIBUTE_TYPE.INTEGER);
+
+        final List<AspectElement> edge_attributes_elements = new ArrayList<AspectElement>();
+        edge_attributes_elements.add(ea0);
+        edge_attributes_elements.add(ea1);
+        edge_attributes_elements.add(ea2);
+        edge_attributes_elements.add(ea3);
+
+        final ArrayList<String> v0 = new ArrayList<String>();
+        v0.add("0.0");
+        v0.add("0.1");
+        final ArrayList<String> v1 = new ArrayList<String>();
+        v1.add("1.0");
+        v1.add("1.1");
+        final ArrayList<String> v2 = new ArrayList<String>();
+        v2.add("2.0");
+        v2.add("2.1");
+
+        final NodeAttributesElement na0 = new NodeAttributesElement("漢字", "한글", v0, ATTRIBUTE_TYPE.DOUBLE);
+        final NodeAttributesElement na1 = new NodeAttributesElement("chữ Quốc ngữ", "БПД", v1, ATTRIBUTE_TYPE.DOUBLE);
+        final NodeAttributesElement na2 = new NodeAttributesElement("カタカナ", "БПД", v2, ATTRIBUTE_TYPE.DOUBLE);
+
+        final ArrayList<String> n = new ArrayList<String>();
+        n.add("漢字");
+        n.add("chữ Quốc ngữ");
+        n.add("カタカナ");
+
+        final ArrayList<String> mm = new ArrayList<String>();
+        mm.add("Mus musculus");
+        final NodeAttributesElement na3 = new NodeAttributesElement("subnet 1", n, "species", mm, ATTRIBUTE_TYPE.STRING);
+
+        final List<AspectElement> node_attributes_elements = new ArrayList<AspectElement>();
+        node_attributes_elements.add(na0);
+        node_attributes_elements.add(na1);
+        node_attributes_elements.add(na2);
+        node_attributes_elements.add(na3);
+
+        // Writing to CX
+        // -------------
+        final OutputStream out = new ByteArrayOutputStream();
+
+        final CxWriter w = CxWriter.createInstanceWithAllAvailableWriters(out, true, true);
+
+        w.start();
+        w.writeAspectElements(edges_elements);
+        w.writeAspectElements(nodes_elements);
+        w.writeAspectElements(cartesian_elements);
+        w.writeAspectElements(edge_attributes_elements);
+        w.writeAspectElements(node_attributes_elements);
+        w.end();
+
+        final String cx_json_str = out.toString();
+
+        final AspectElementCounts cw = w.getAspectElementCounts();
+
+        System.out.println(cx_json_str);
+
+        // Reading from CX
+        // ---------------
+        final Set<AspectFragmentReader> readers = new HashSet<>();
+
+        final EdgesFragmentReader er = EdgesFragmentReader.createInstance();
+
+        readers.add(er);
+        readers.add(NodesFragmentReader.createInstance());
+        readers.add(CartesianLayoutFragmentReader.createInstance());
+        readers.add(EdgeAttributesFragmentReader.createInstance());
+        readers.add(NodeAttributesFragmentReader.createInstance());
+        final CxReader p = CxReader.createInstance(cx_json_str, true, true, readers);
+
+        while (p.hasNext()) {
+            final List<AspectElement> elements = p.getNext();
+            if (!elements.isEmpty()) {
+                final String aspect_name = elements.get(0).getAspectName();
+                System.out.println();
+                System.out.println(aspect_name + ": ");
+                for (final AspectElement element : elements) {
+                    System.out.println(element.toString());
+                }
+            }
+        }
+
+        final AspectElementCounts cr = p.getAspectElementCounts();
+        System.out.println(cr);
+        Util.validate(w.getMd5Checksum(), p.getMd5Checksum(), cw, cr);
+
+    }
+
+}

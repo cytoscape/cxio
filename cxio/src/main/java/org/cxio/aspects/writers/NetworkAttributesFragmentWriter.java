dca4bd3a20d0f1d3517270f1f11e77a26738b08e
cmzmasek <cmzmasek@yahoo.com>
2015-11-16 18:17:07 -0800

    bug fix

--- cxio/src/main/java/org/cxio/aspects/writers/NetworkAttributesFragmentWriter.java
+++ cxio/src/main/java/org/cxio/aspects/writers/NetworkAttributesFragmentWriter.java
@@ -21,7 +21,6 @@ public class NetworkAttributesFragmentWriter extends AbstractFragmentWriter {
 
     @Override
     public void writeElement(final AspectElement element, final JsonWriter w) throws IOException {
-
         WriterUtil.writeAttributesElement(w, (NetworkAttributesElement) element, _filter, false);
     }
 

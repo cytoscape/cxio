f9ace271b00253367878ae85f1e3ffa6b907f45d
cmzmasek <cmzmasek@yahoo.com>
2016-01-28 18:34:03 -0800

    allow to force "symmetrical" network (interactions)

--- src/main/java/org/cxio/cmd/Edgelist2Cx.java
+++ src/main/java/org/cxio/cmd/Edgelist2Cx.java
@@ -5,8 +5,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
@@ -23,7 +25,7 @@ import org.cxio.tools.BasicTableParser;
  *
  * Simple usage as command line:
  *
- * java -cp path/to/cxio-x.x.x.jar org.cxio.cmd.Edgelist2Cx infile outfile
+ * java -cp path/to/cxio-x.x.x.jar org.cxio.cmd.Edgelist2Cx -f infile outfile
  *
  *
  */
@@ -34,25 +36,40 @@ public final class Edgelist2Cx {
 
     public static void main(final String[] args) throws IOException {
 
-        if (args.length != 2) {
-            System.out.println("Usage: Edgelist2Cx <infile: tab-separated edge-list> <outfile: network in cx-format>");
-            System.exit(-1);
+        File infile = null;
+        File outfile = null;
+        boolean force_symetrical = false;
+        if (args.length == 2) {
+            infile = new File(args[0]);
+            outfile = new File(args[1]);
         }
-
-        final File infile = new File(args[0]);
-        final File outfile = new File(args[1]);
+        else if (args.length == 3) {
+            infile = new File(args[1]);
+            outfile = new File(args[2]);
+            if (args[0].equals("-f")) {
+                force_symetrical = true;
+            }
+            else {
+                error();
+            }
+        }
+        else {
+            error();
+        }
 
         if (!infile.exists()) {
             System.out.println("does not exist: " + infile);
             System.exit(-1);
         }
+
         if (outfile.exists()) {
             System.out.println("already exists: " + outfile);
             System.exit(-1);
         }
 
-        System.out.println("Infile : " + infile);
-        System.out.println("Outfile: " + outfile);
+        System.out.println("Infile           : " + infile);
+        System.out.println("Outfile          : " + outfile);
+        System.out.println("Force symmetrical: " + force_symetrical);
         System.out.println();
 
         final BasicTable<String> t = BasicTableParser.parse(infile, COLUMN_DELIMITER);
@@ -93,14 +110,24 @@ public final class Edgelist2Cx {
 
         for (final Entry<Integer, String> e : id_to_node.entrySet()) {
             cx_nodes.add(new NodesElement(e.getKey(), e.getValue()));
-            // cx_node_attributes.add(new
-            // NodeAttributesElement(Long.valueOf(e.getKey()), NAME,
-            // e.getValue()));
         }
+        if (!force_symetrical) {
+            for (int r = 0; r < t.getNumberOfRows(); ++r) {
+                cx_edges.add(new EdgesElement(r, node_to_id.get(t.getValue(0, r)), node_to_id.get(t.getValue(1, r))));
 
-        for (int r = 0; r < t.getNumberOfRows(); ++r) {
-            cx_edges.add(new EdgesElement(r, node_to_id.get(t.getValue(0, r)), node_to_id.get(t.getValue(1, r))));
-            cx_edge_attributes.add(new EdgeAttributesElement(Long.valueOf(r), WEIGHT, t.getValue(2, r)));
+                cx_edge_attributes.add(new EdgeAttributesElement(Long.valueOf(r), WEIGHT, t.getValue(2, r)));
+            }
+        }
+        else {
+            final Set<String> egdes_map = new HashSet<String>();
+            int counter = 0;
+            for (int r = 0; r < t.getNumberOfRows(); ++r) {
+                final Integer node0 = node_to_id.get(t.getValue(0, r));
+                final Integer node1 = node_to_id.get(t.getValue(1, r));
+                final String w = t.getValue(2, r);
+                counter = addSymmetricalEdges(cx_edges, cx_edge_attributes, egdes_map, counter, node0, node1, w);
+                counter = addSymmetricalEdges(cx_edges, cx_edge_attributes, egdes_map, counter, node1, node0, w);
+            }
         }
 
         final int id_counter = cx_nodes.size() + cx_node_attributes.size() + cx_edges.size() + cx_edge_attributes.size();
@@ -124,5 +151,29 @@ public final class Edgelist2Cx {
 
         System.out.println("OK");
 
+    }
+
+    private static void error() {
+
+        System.out.println("Usage: Edgelist2Cx [-f] <infile: tab-separated edge-list> <outfile: network in cx-format>");
+        System.exit(-1);
+
+    }
+
+    private final static int addSymmetricalEdges(final List<AspectElement> cx_edges,
+                                                 final List<AspectElement> cx_edge_attributes,
+                                                 final Set<String> egdes_map,
+                                                 int counter,
+                                                 final Integer node0,
+                                                 final Integer node1,
+                                                 final String weight) {
+        final String edge_as_str = String.valueOf(node0) + String.valueOf(node1);
+        if (!egdes_map.contains(edge_as_str)) {
+            cx_edges.add(new EdgesElement(counter, node0, node1));
+            cx_edge_attributes.add(new EdgeAttributesElement(Long.valueOf(counter), WEIGHT, weight));
+            egdes_map.add(edge_as_str);
+            ++counter;
+        }
+        return counter;
     }
 }
